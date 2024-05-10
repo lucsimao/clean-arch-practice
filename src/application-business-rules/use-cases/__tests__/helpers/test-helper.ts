123208701151b77fc7b21b58a818b5a4663f96d1
lucsimao <lucas64_64@hotmail.com>
2024-05-09 21:20:28 -0300

    feat: update test

--- src/application-business-rules/use-cases/__tests__/helpers/test-helper.ts
+++ src/application-business-rules/use-cases/__tests__/helpers/test-helper.ts
@@ -1,17 +1,34 @@
-import {
-  Order,
-  Status,
-} from '../../../../enterprise-business-rules/entities/Order';
-import {
-  CreateOrder,
-  CreateOrderInput,
-} from '../../../../enterprise-business-rules/__protocols__/CreateProcess';
-import {
-  CreateOrderController,
-  CreateOrderControllerInput,
-} from '../../../../interface-adapters/controllers/CreateOrderController';
 import { CreateOrderRepository } from '../../../__protocols__/CreateOrderRepository';
-import { CreateOrderUseCaseInput } from '../../CreateOrderUseCase';
+import { CreateOrder } from '../../../../enterprise-business-rules/__protocols__/CreateProcess';
+import { Status } from '../../../../enterprise-business-rules/entities/Order';
+import { CreateOrderControllerInput } from '../../../../interface-adapters/controllers/CreateOrderController';
+import {
+  CreateOrderUseCaseInput,
+  CreateOrderUseCaseOutput,
+} from '../../CreateOrderUseCase';
+
+export const makeCreateOrderControllerInput =
+  (): CreateOrderControllerInput => ({
+    id: 1,
+    items: [],
+    restaurantId: '2',
+    status: Status.IN_PROGRESS,
+    table: 3,
+  });
+
+export const makeCreateOrderStub = (): jest.Mocked<CreateOrder> => ({
+  execute: jest.fn().mockResolvedValue(undefined),
+});
+
+export const makeFakeCreateOrderUseCaseOutput =
+  (): jest.Mocked<CreateOrderUseCaseOutput> => {
+    return undefined;
+  };
+
+export const makeCreateOrderRepositoryStub =
+  (): jest.Mocked<CreateOrderRepository> => ({
+    create: jest.fn().mockResolvedValue(makeFakeCreateOrderUseCaseOutput()),
+  });
 
 export const makeFakeCreateOrderUseCaseInput = (
   payload?: Partial<CreateOrderUseCaseInput>
@@ -21,53 +38,5 @@ export const makeFakeCreateOrderUseCaseInput = (
   restaurantId: 'Pizzaria torre de Pizza',
   status: Status.IN_PROGRESS,
   table: 4,
-  ...(payload as any),
-});
-
-export const makeFakeCreateOrder = (
-  payload?: Partial<CreateOrder>
-): jest.Mocked<CreateOrder> => ({
-  id: 1,
-  items: [],
-  restaurantId: 'Pizzaria torre de Pizza',
-  status: Status.IN_PROGRESS,
-  table: 4,
-  ...(payload as any),
-});
-
-export const makeFakeapiHttpRequest = ({
-  body,
-  params,
-  headers,
-  user,
-  query,
-}: {
-  body?: unknown;
-  params?: { [key: string]: unknown };
-  headers?: { [key: string]: string | string[] | undefined };
-  user?: unknown;
-  query?: { [key: string]: unknown };
-}): jest.Mocked<any> => ({
-  body,
-  params,
-  headers,
-  user,
-  query,
-});
-
-export const makeFakeApiHttpResponse = (
-  status: keyof typeof Status,
-  body?: unknown
-): jest.Mocked<any> => ({
-  body,
-  status: status || 'OK',
-});
-
-export const makeCreateOrderUserStub =
-  (): jest.Mocked<CreateOrderRepository> => ({
-    create: jest.fn().mockResolvedValue(makeFakeCreateOrderUseCaseInput()),
-  });
-
-export const makeCreateOrder = (): jest.Mocked<CreateOrder> => ({
-  execute: jest.fn().mockResolvedValue(makeFakeCreateOrder()),
+  ...payload,
 });
